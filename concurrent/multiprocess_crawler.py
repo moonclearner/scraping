e90060db2b874ca550ac145bdc7c2bebd7973a44
moonclearner <718857460@qq.com>
2017-07-20 22:04:55 +0800

    add  concurrent

--- concurrent/multiprocess_crawler.py
+++ concurrent/multiprocess_crawler.py
@@ -1,31 +1,30 @@
 import time
-import threading
 import urlparse
-from scraping.cache.Downloader import Downloader
+import threading
+import multiprocessing
+from scraping.cache.mongo_cache import MongoCache
 from mongo_queue import MongoQueue
-
+from scraping.cache.Downloader import Downloader
 
 SLEEP_TIME = 1
 
 
 def threaded_crawler(seed_url, delay=5, cache=None, scrape_callback=None, user_agent='wswp', proxies=None, num_retries=1, max_threads=10, timeout=60):
-    """Crawl this website in multiple threads
+    """Crawl using multiple threads
     """
     # the queue of URL's that still need to be crawled
-    # crawl_queue = Queue.deque([seed_url])
-    #  crawl_queue = [seed_url]
     crawl_queue = MongoQueue()
+    crawl_queue.clear()
     crawl_queue.push(seed_url)
-    # the URL's that have been seen
-    seen = set([seed_url])
     D = Downloader(cache=cache, delay=delay, user_agent=user_agent, proxies=proxies, num_retries=num_retries, timeout=timeout)
 
     def process_queue():
         while True:
+            # keep track that are processing url
             try:
                 url = crawl_queue.pop()
-            except IndexError:
-                # crawl queue is empty
+            except KeyError:
+                # currently no urls to process
                 break
             else:
                 html = D(url)
@@ -36,37 +35,43 @@ def threaded_crawler(seed_url, delay=5, cache=None, scrape_callback=None, user_a
                         print 'Error in callback for: {}: {}'.format(url, e)
                     else:
                         for link in links:
-                            link = normalize(seed_url, link)
-                            # check whether already crawled this link
-                            if link not in seen:
-                                seen.add(link)
-                                # add this new link to queue
-                                crawl_queue.complete(link)
-                                #  crawl_queue.append(link)
+                            # add this new link to queue
+                            crawl_queue.push(normalize(seed_url, link))
+                crawl_queue.complete(url)
+
 
     # wait for all download threads to finish
     threads = []
     while threads or crawl_queue:
-        # the crawl is still active
         for thread in threads:
             if not thread.is_alive():
-                # remove the stopped threads
                 threads.remove(thread)
-        while len(threads) < max_threads and crawl_queue:
+        while len(threads) < max_threads and crawl_queue.peek():
             # can start some more threads
             thread = threading.Thread(target=process_queue)
-            thread.setDaemon(True)
-            # set daemon so main thread can exit when receives ctrl-c
+            thread.setDaemon(True) # set daemon so main thread can exit when receives ctrl-c
             thread.start()
             threads.append(thread)
-        # all threads have been processed
-        # sleep temporarily so CPU can focus execution on other threads
         time.sleep(SLEEP_TIME)
+
+
+def process_crawler(args, **kwargs):
+    num_cpus = multiprocessing.cpu_count()
+    #pool = multiprocessing.Pool(processes=num_cpus)
+    print 'Starting {} processes'.format(num_cpus)
+    processes = []
+    for i in range(num_cpus):
+        p = multiprocessing.Process(target=threaded_crawler, args=[args], kwargs=kwargs)
+        #parsed = pool.apply_async(threaded_link_crawler, args, kwargs)
+        p.start()
+        processes.append(p)
+    # wait for processes to complete
+    for p in processes:
+        p.join()
 
 
 def normalize(seed_url, link):
     """Normalize this URL by removing hash and adding domain
     """
-    link, _ = urlparse.urldefrag(link)
-    # remove hash to avoid duplicates
+    link, _ = urlparse.urldefrag(link) # remove hash to avoid duplicates
     return urlparse.urljoin(seed_url, link)
